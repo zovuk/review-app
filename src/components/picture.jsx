9753813cdc82b1f80168122e51d312270c02ddb1
Zoran Vukajlic <zoranvukajlic@gmail.com>
2021-03-01 14:27:57 +0100

    picture showing only if available

--- src/components/picture.jsx
+++ src/components/picture.jsx
@@ -2,23 +2,46 @@ import React, { Component } from 'react';
 import { theKey } from './apiKey';
 
 class Picture extends Component {
+  state = {
+    toRender: '',
+  };
+
+  async componentDidMount() {
+    let response = await fetch(this.srcURL('/metadata?'));
+
+    if (response.ok) {
+      // if HTTP-status is 200-299
+      let json = await response.json();
+      if (json.status === 'OK') {
+        this.setState({
+          toRender: (
+            <div>
+              <img
+                src={this.srcURL('?size=600x600&')}
+                className="img-fluid border rounded mb-3"
+                alt=""
+              ></img>
+            </div>
+          ),
+        });
+      }
+    } else {
+      alert('HTTP-Error: ' + response.status);
+    }
+  }
+
+  srcURL = (ext) =>
+    'https://maps.googleapis.com/maps/api/streetview' +
+    ext +
+    'location=' +
+    this.props.selectedRestaurantCoors.lat +
+    ',' +
+    this.props.selectedRestaurantCoors.lng +
+    '&key=' +
+    theKey;
+
   render() {
-    const srcURL =
-      'https://maps.googleapis.com/maps/api/streetview?size=600x600&location=' +
-      this.props.selectedRestaurantCoors.lat +
-      ',' +
-      this.props.selectedRestaurantCoors.lng +
-      '&key=' +
-      theKey;
-    return (
-      <div>
-        <img
-          src={srcURL}
-          className="img-fluid border rounded mb-3"
-          alt=""
-        ></img>
-      </div>
-    );
+    return <React.Fragment>{this.state.toRender}</React.Fragment>;
   }
 }
 
